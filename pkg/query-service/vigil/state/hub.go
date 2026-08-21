e817b54f557a478c5e2e219a47e8929c37ff592c
Aaditya1273 <aadityaiitian1515@gmail.com>
2026-08-21 05:34:54 +0530

    fix: send each ws message as a separate frame to prevent JSON.parse errors

--- pkg/query-service/vigil/state/hub.go
+++ pkg/query-service/vigil/state/hub.go
@@ -167,21 +167,16 @@ func (c *Client) writePump() {
 				return
 			}
 
-			w, err := c.conn.NextWriter(websocket.TextMessage)
-			if err != nil {
+			if err := c.conn.WriteMessage(websocket.TextMessage, message); err != nil {
 				return
 			}
-			w.Write(message)
 
-			// Add queued chat messages to the current websocket message.
+			// Send queued messages in separate websocket frames to avoid breaking JSON.parse on the client.
 			n := len(c.send)
 			for i := 0; i < n; i++ {
-				w.Write([]byte{'\n'})
-				w.Write(<-c.send)
-			}
-
-			if err := w.Close(); err != nil {
-				return
+				if err := c.conn.WriteMessage(websocket.TextMessage, <-c.send); err != nil {
+					return
+				}
 			}
 		case <-ticker.C:
 			c.conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
